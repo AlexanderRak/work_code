7bdf0a7968b6d35642901ed20a4c26687a816770
Alehander <sashok.rak@gmail.com>
2020-12-01 17:38:14 +0200

    01 12 push 2

--- 3. work_gitlab_rev/Софт на любой девайс/new/soft-na-ljuboj-devajs.js
+++ 3. work_gitlab_rev/Софт на любой девайс/new/soft-na-ljuboj-devajs.js
@@ -7,28 +7,24 @@ function getCookie(name) {
    return matches ? decodeURIComponent(matches[1]) : undefined;
 }
 var city = getCookie('active_city_id');
-var priceUrl = 'https://comfy.ua/online_remains/product/ritail_rockets_price.php?city_id=' + city + '&ids=762023,149473,413773,413783,413793,149463,413803,413813,413823,762033,762043,690863,690873,774093,774143,690883,690893,774133,774113,774123,683043,683033,125053,125063,125103,424493';
+var priceUrl = 'https://comfy.ua/online_remains/product/ritail_rockets_price.php?city_id=' + city + '&ids=1522633,1522643,1522653,1522663,1522673,1522683,1522693,1522703,1522713,762033,762043,690883,690893,774133,774113,774123,683043,683033,125053,125063,125103,424493';
 $.ajax({
    url: priceUrl,
    type: 'get',
    success: function (output) {
       var resp = JSON.parse(output);
 
-      var pr1 = resp['762023']['price'];
-      var pr2 = resp['149473']['price'];
-      var pr3 = resp['413773']['price'];
-      var pr4 = resp['413783']['price'];
-      var pr5 = resp['413793']['price'];
-      var pr6 = resp['149463']['price'];
-      var pr7 = resp['413803']['price'];
-      var pr8 = resp['413813']['price'];
-      var pr9 = resp['413823']['price'];
+      var pr1 = resp['1522633']['price'];
+      var pr2 = resp['1522643']['price'];
+      var pr3 = resp['1522653']['price'];
+      var pr4 = resp['1522663']['price'];
+      var pr5 = resp['1522673']['price'];
+      var pr6 = resp['1522683']['price'];
+      var pr7 = resp['1522693']['price'];
+      var pr8 = resp['1522703']['price'];
+      var pr9 = resp['1522713']['price'];
       var pr10 = resp['762033']['price'];
       var pr11 = resp['762043']['price'];
-      var pr12 = resp['690863']['price'];
-      var pr13 = resp['690873']['price'];
-      var pr14 = resp['774093']['price'];
-      var pr15 = resp['774143']['price'];
       var pr16 = resp['690883']['price'];
       var pr17 = resp['690893']['price'];
       var pr18 = resp['774133']['price'];
@@ -42,21 +38,17 @@ $.ajax({
       var pr29 = resp['424493']['price'];
       
       
-      document.getElementById("sku-1247684").innerHTML = pr1;
-      document.getElementById("sku-1247686").innerHTML = pr2;
-      document.getElementById("sku-1390946").innerHTML = pr3;
-      document.getElementById("sku-1390948").innerHTML = pr4;
-      document.getElementById("sku-1390950").innerHTML = pr5;
-      document.getElementById("sku-1247685").innerHTML = pr6;
-      document.getElementById("sku-1390951").innerHTML = pr7;
-      document.getElementById("sku-1390952").innerHTML = pr8;
-      document.getElementById("sku-1390953").innerHTML = pr9;
+      document.getElementById("sku-1700016").innerHTML = pr1;
+      document.getElementById("sku-1700018").innerHTML = pr2;
+      document.getElementById("sku-1700020").innerHTML = pr3;
+      document.getElementById("sku-1700022").innerHTML = pr4;
+      document.getElementById("sku-1700023").innerHTML = pr5;
+      document.getElementById("sku-1700029").innerHTML = pr6;
+      document.getElementById("sku-1700031").innerHTML = pr7;
+      document.getElementById("sku-1700033").innerHTML = pr8;
+      document.getElementById("sku-1700034").innerHTML = pr9;
       document.getElementById("sku-1538643").innerHTML = pr10;
       document.getElementById("sku-1538644").innerHTML = pr11;
-      document.getElementById("sku-1514829").innerHTML = pr12;
-      document.getElementById("sku-1514830").innerHTML = pr13;
-      document.getElementById("sku-1542177").innerHTML = pr14;
-      document.getElementById("sku-1542189").innerHTML = pr15;
       document.getElementById("sku-1514833").innerHTML = pr16;
       document.getElementById("sku-1514834").innerHTML = pr17;
       document.getElementById("sku-1542188").innerHTML = pr18;
