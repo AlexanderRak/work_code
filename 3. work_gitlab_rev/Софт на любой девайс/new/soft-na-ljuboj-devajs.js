3f70a6e30d0987cb239a21478ebd73298f5bb23b
Alehander <sashok.rak@gmail.com>
2021-08-03 17:37:38 +0300

    03 08 push

--- 3. work_gitlab_rev/Софт на любой девайс/new/soft-na-ljuboj-devajs.js
+++ 3. work_gitlab_rev/Софт на любой девайс/new/soft-na-ljuboj-devajs.js
@@ -7,7 +7,7 @@ function getCookie(name) {
    return matches ? decodeURIComponent(matches[1]) : undefined;
 }
 var city = getCookie('active_city_id');
-var priceUrl = 'https://comfy.ua/online_remains/product/ritail_rockets_price.php?city_id=' + city + '&ids=1522683,1522693,762033,762043,683043,683033,125053,125063,125103,424493';
+var priceUrl = 'https://comfy.ua/online_remains/product/ritail_rockets_price.php?city_id=' + city + '&ids=1522683,1522693,683043,683033,125053,125063,125103,424493';
 $.ajax({
    url: priceUrl,
    type: 'get',
@@ -16,8 +16,6 @@ $.ajax({
 
       var pr6 = resp['1522683']['price'];
       var pr7 = resp['1522693']['price'];
-      var pr10 = resp['762033']['price'];
-      var pr11 = resp['762043']['price'];
       var pr24 = resp['683033']['price'];
       var pr25 = resp['683043']['price'];
       var pr26 = resp['125053']['price'];
@@ -27,8 +25,6 @@ $.ajax({
       
       document.getElementById("sku-1700029").innerHTML = pr6;
       document.getElementById("sku-1700031").innerHTML = pr7;
-      document.getElementById("sku-1538643").innerHTML = pr10;
-      document.getElementById("sku-1538644").innerHTML = pr11;
       document.getElementById("sku-1444165").innerHTML = pr24;
       document.getElementById("sku-1444167").innerHTML = pr25;
       document.getElementById("sku-1227118").innerHTML = pr26;
@@ -116,26 +112,6 @@ $.ajax({
       document.getElementById("sku-1696532").innerHTML = pr36;
       document.getElementById("sku-1696535").innerHTML = pr40;
       document.getElementById("sku-1696537").innerHTML = pr41;
-
-   }
-});
-
-var priceUrl34 = 'https://comfy.ua/online_remains/product/ritail_rockets_price.php?city_id=' + city + '&ids=1248513,1248523,1373683,1373693';
-$.ajax({
-   url: priceUrl34,
-   type: 'get',
-   success: function (output) {
-      var resp = JSON.parse(output);
-
-      var pr36 = resp['1248513']['price'];
-      var pr40 = resp['1248523']['price'];
-      var pr41 = resp['1373683']['price'];
-      var pr42 = resp['1373693']['price'];
-      
-      document.getElementById("sku-1663203").innerHTML = pr36;
-      document.getElementById("sku-1663204").innerHTML = pr40;
-      document.getElementById("sku-1663205").innerHTML = pr41;
-      document.getElementById("sku-1663206").innerHTML = pr42;
 
    }
 });
--- 3. work_gitlab_rev/Софт на любой девайс/new/Онлайн кинотеатры и ТВ подписки/soft-na-ljuboj-devajs.js
+++ 3. work_gitlab_rev/Софт на любой девайс/new/Онлайн кинотеатры и ТВ подписки/soft-na-ljuboj-devajs.js
@@ -7,7 +7,7 @@ function getCookie(name) {
    return matches ? decodeURIComponent(matches[1]) : undefined;
 }
 var city = getCookie('active_city_id');
-var priceUrl = 'https://comfy.ua/online_remains/product/ritail_rockets_price.php?city_id=' + city + '&ids=1522683,1522693,762033,762043,683033,683043';
+var priceUrl = 'https://comfy.ua/online_remains/product/ritail_rockets_price.php?city_id=' + city + '&ids=1522683,1522693,683033,683043';
 $.ajax({
    url: priceUrl,
    type: 'get',
@@ -16,15 +16,11 @@ $.ajax({
 
       var pr6 = resp['1522683']['price'];
       var pr7 = resp['1522693']['price'];
-      var pr10 = resp['762033']['price'];
-      var pr11 = resp['762043']['price'];
       var pr24 = resp['683033']['price'];
       var pr25 = resp['683043']['price'];
 
       document.getElementById("sku-1700029").innerHTML = pr6;
       document.getElementById("sku-1700031").innerHTML = pr7;
-      document.getElementById("sku-1538643").innerHTML = pr10;
-      document.getElementById("sku-1538644").innerHTML = pr11;
       document.getElementById("sku-1444165").innerHTML = pr24;
       document.getElementById("sku-1444167").innerHTML = pr25;
 
@@ -52,25 +48,6 @@ $.ajax({
    }
 });
 
-var priceUrl34 = 'https://comfy.ua/online_remains/product/ritail_rockets_price.php?city_id=' + city + '&ids=1248513,1248523,1373683,1373693';
-$.ajax({
-   url: priceUrl34,
-   type: 'get',
-   success: function (output) {
-      var resp = JSON.parse(output);
-
-      var pr36 = resp['1248513']['price'];
-      var pr40 = resp['1248523']['price'];
-      var pr41 = resp['1373683']['price'];
-      var pr42 = resp['1373693']['price'];
-      
-      document.getElementById("sku-1663203").innerHTML = pr36;
-      document.getElementById("sku-1663204").innerHTML = pr40;
-      document.getElementById("sku-1663205").innerHTML = pr41;
-      document.getElementById("sku-1663206").innerHTML = pr42;
-
-   }
-});
 
 var priceUrl341megogo = 'https://comfy.ua/online_remains/product/ritail_rockets_price.php?city_id=' + city + '&ids=2529353,2529363,2529373';
 $.ajax({
