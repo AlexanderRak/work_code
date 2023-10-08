eda4871ac6b7142f72cf80457d3cf5fe86b1de63
1erste <56805918+1erste@users.noreply.github.com>
2023-10-08 14:04:27 +0300

    not ready

---
+++ 4. Vlad_code/not ready/Rich - WMF Perfection FACM 660/Untitled-1.js
@@ -0,0 +1,31 @@
+// if (window.innerWidth >= 1341) {
+            //    tl.fromTo(cleanOne, {opacity: 1}, {opacity: 0}, 0).fromTo(cleanTwo, {opacity: 0}, {opacity: 1}, 0);
+            //    ScrollTrigger.create({
+            //       animation: tl,
+            //       trigger: ".clean",
+            //       scrub: true,
+            //       start: "top 10%",
+            //       end: "+=50%",
+            //       id: "clean",
+            //    });
+            // } else if (window.innerWidth <= 1340 && window.innerWidth >= 1025) {
+            //    tl.fromTo(cleanOne, {opacity: 1}, {opacity: 0}, 0).fromTo(cleanTwo, {opacity: 0}, {opacity: 1}, 0);
+            //    ScrollTrigger.create({
+            //       animation: tl,
+            //       trigger: ".clean",
+            //       scrub: true,
+            //       start: "top -20%",
+            //       end: "+=50%",
+            //       id: "clean",
+            //    });
+            // } else if (window.innerWidth >= 991 && window.innerWidth <= 1024) {
+            //    tl.fromTo(cleanOne, {opacity: 1}, {opacity: 0}, 0).fromTo(cleanTwo, {opacity: 0}, {opacity: 1}, 0);
+            //    ScrollTrigger.create({
+            //       animation: tl,
+            //       trigger: ".clean",
+            //       scrub: true,
+            //       start: "top 10%",
+            //       end: "+=50%",
+            //       id: "clean",
+            //    });
+            // }
